dbd677e44c05d21544c5201aaa13119167b95849
Krsikapa00 <krsinich589@gmail.com>
2023-11-13 22:45:54 -0500

    added and updated files to get model to train need to confirm loss function is working

---
+++ Lab4/src/KiitiROIDataset.py
@@ -0,0 +1,92 @@
+import os
+import fnmatch
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms, models
+import cv2
+
+class KittiROIDataset(Dataset):
+    def __init__(self, dir, training=True, transform=None):
+        self.dir = dir
+        self.training = training
+        self.mode = 'train'
+        self.target_transform = lambda data: torch.tensor(data, dtype=torch.int)
+        if self.training == False:
+            self.mode = 'test'
+        self.img_dir = os.path.join(dir)
+        self.label_dir = os.path.join(dir)
+        self.transform = transform
+        self.num = 0
+        self.img_files = []
+        for file in os.listdir(self.img_dir):
+            if fnmatch.fnmatch(file, '*.png'):
+                self.img_files += [file]
+
+        self.max = len(self)
+
+        # print('break 12: ', self.img_dir)
+        # print('break 12: ', self.label_dir)
+
+    def __len__(self):
+        return len(self.img_files)
+
+    def __getitem__(self, idx):
+        #TODO NICK: Update this to return label and img correctly
+        '''
+        Get img number from name
+        open labels.txt file from directory
+        iterate through each line in file
+        Find line that starts w image name
+        Check what value is on the line. return the integer (0, 1)
+        '''
+        img_name = os.path.splitext(self.img_files[idx])[0]
+        # print("IMAGE NAME: {}".format(img_name))
+        img_path = os.path.join(self.img_dir, self.img_files[idx])
+        image = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        if self.transform is not None:
+            image = self.transform(image)
+
+        label = None
+        label_path = os.path.join(self.label_dir, 'labels.txt')
+        foundLabel = None
+        with open(label_path) as label_file:
+            while True:
+                line = label_file.readline()
+
+                if line.startswith(img_name):
+                    foundLabel = line
+                    break
+                elif not line:
+                    foundLabel = None
+                    break
+
+        if foundLabel is None:
+            # print("Could not find a label for this ROI image. Please check labels.txt file")
+            foundLabel = None
+        else:
+            # print("Found label:   {}".format(foundLabel))
+            split_label = foundLabel.split(" ")
+            label = int(split_label[1])
+        return image, label
+
+    def __iter__(self):
+        self.num = 0
+        return self
+
+    def __next__(self):
+        if (self.num >= self.max):
+            raise StopIteration
+        else:
+            self.num += 1
+            return self.__getitem__(self.num-1)
+
+
+    def strip_ROIs(self, class_ID, label_list):
+        ROIs = []
+        for i in range(len(label_list)):
+            ROI = label_list[i]
+            if ROI[1] == class_ID:
+                pt1 = (int(ROI[3]),int(ROI[2]))
+                pt2 = (int(ROI[5]), int(ROI[4]))
+                ROIs += [(pt1,pt2)]
+        return ROIs
---
+++ Lab4/src/train.py
@@ -0,0 +1,236 @@
+import argparse
+import pickle
+import ssl
+import os
+
+import torch.optim as optim
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import torch
+import torchvision.transforms as transforms
+import time as t
+
+import YodaModel as model
+import KiitiROIDataset as KittiData
+
+
+
+def get_output_accuracy(pred, confirmed, top_res=(1,)):
+    max_num_results = max(top_res) #get highest result looking for
+    data_size = confirmed.size(0)
+
+    pred_values, pred_indx = pred.topk(k=max_num_results, dim=1)
+    pred_indx = pred_indx.t()
+#   Reshape confirmed indxs
+    confirmed_indx_reshape = confirmed.view(1, -1).expand_as(pred_indx)
+    answers = pred_indx == confirmed_indx_reshape
+
+    top_res_acc = []
+    for k in top_res:
+        temp_answers = answers[:k] #limit each image answer to k values
+        temp_answers = temp_answers.reshape(-1).float() #flatten to see whats right form ALL images in batch
+        temp_answers = temp_answers.float().sum(dim=0, keepdim=True)
+        curr_acc = temp_answers/data_size
+        top_res_acc.append(curr_acc)
+    return top_res_acc
+def evaluate_epoch_top1_5(model, data, device, test_loader=None):
+    model.eval() #Set to evaluate
+    tot_top1_accuracy = 0
+    tot_top5_accuracy = 0
+    test_tot_top1_accuracy = 0
+    test_tot_top5_accuracy = 0
+    for imgs, labels in data:
+        imgs = imgs.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad():
+            output = model(imgs)
+        curr_top1, curr_top5 = get_output_accuracy(output, labels, (1, 5))
+        tot_top1_accuracy += curr_top1
+        tot_top5_accuracy += curr_top5
+    avg_test_top5_epoch_acc = 0
+    avg_test_top1_epoch_acc = 0
+    if test_loader is not None:
+        for imgs, labels in test_loader:
+            imgs = imgs.to(device)
+            labels = labels.to(device)
+
+            with torch.no_grad():
+                output = model(imgs)
+            curr_top1, curr_top5 = get_output_accuracy(output, labels, (1, 5))
+            test_tot_top1_accuracy += curr_top1
+            test_tot_top5_accuracy += curr_top5
+    avg_test_top5_epoch_acc = test_tot_top5_accuracy / len(test_loader)
+    avg_test_top1_epoch_acc = test_tot_top1_accuracy / len(test_loader)
+    avg_top5_epoch_acc = tot_top5_accuracy / len(data)
+    avg_top1_epoch_acc = tot_top1_accuracy / len(data)
+
+    model.train() #Set to train when returning to function
+
+    return avg_top1_epoch_acc, avg_top5_epoch_acc, avg_test_top1_epoch_acc, avg_test_top5_epoch_acc
+
+def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler, device,
+          encoder_save=None, plot_file=None, pickleLosses = None,
+          starting_epoch=1, evaluate_epochs=False, folder='./', store_data=False, test_loader=None):
+
+    model.train()
+    total_losses = []
+
+    final_loss = 0.0
+    t_1 = t.time()
+    print("\n=======================================")
+    print("Training started at Epoch {}\n".format(starting_epoch))
+
+    # loading_saved pickle data
+    # if pickleLosses is not None:
+    #     try:
+    #         with open(pickleLosses, 'rb') as file:
+    #             loaded_losses = pickle.load(file)
+    #             (total_losses, content_losses, style_losses) = loaded_losses
+    #             print("Loaded saved losses from file successfully: \n{} \n{} \n{}"
+    #                   .format(total_losses, content_losses, style_losses))
+    #     except Exception as e:
+    #         print(f"An error occurred while loading arrays: {str(e)}")
+
+
+    for epoch in range(starting_epoch, n_epochs + 1):
+        print("Starting Epoch: ", epoch)
+        # Losses for current epoch
+        total_loss = 0.0
+        t_2 = t.time()
+
+        for idx, data in enumerate(train_loader):
+            t_3 = t.time()
+            imgs, labels = data[0].to(device=device), data[1].to(device=device)
+            # print("LEngth of batch: {}".format(len(imgs)))
+            optimizer.zero_grad()
+
+            output_labels = model(imgs)
+            loss = loss_fn(output_labels, labels)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+            print('Batch #{}/{}         Time: {}'.format(idx + 1, len(train_loader), (t.time() - t_3)))
+
+            # if idx == 2:
+            #     break
+        if encoder_save is not None:
+            torch.save(model.encoder.state_dict(), str(epoch) + '_' + encoder_save)
+            print("Saved frontend model under name: {}".format(encoder_save))
+
+        scheduler.step(total_loss)
+        total_losses.append(total_loss / len(train_loader))
+        final_loss = total_loss / len(train_loader)
+
+
+        # Plot loss data each epoch
+        if plot_file is not None:
+            plot_save = os.path.join(os.path.abspath(folder), plot_file)
+            plt.figure(2, figsize=(12, 7))
+            plt.clf()
+            plt.plot(total_losses, label='Total')
+            plt.xlabel('epoch')
+            plt.ylabel('loss')
+            plt.legend(loc=1)
+            plt.savefig(plot_save)
+
+        print('Epoch {}, Training loss {}, Time  {}'.format(epoch, final_loss,(t.time() - t_2)))
+        # if evaluate_epochs:
+
+    print('Total Training loss {}, Time  {}'.format(final_loss, (t.time() - t_1)))
+
+    return final_loss
+
+
+if __name__ == '__main__':
+
+    # ssl._create_default_https_context = ssl._create_unverified_context
+
+    device = 'cpu'
+    # Setup parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dir', '--dir', type=str, required=True,
+                        help='Directory path where all the Kitti_ROIs train and test images are held, dont include train or test folder')
+    # training options
+    parser.add_argument('-e', '--e', type=int, default=40)
+    parser.add_argument('-b', '--b', type=int, default=48, help="Batch size")
+    parser.add_argument('-l', '--l', help="filename to load saved encoder; Encoder.pth", required=False)
+    parser.add_argument('-s', '--s', help="filename to save encoder")
+    parser.add_argument('-p', '--p', help="decoder.png")
+    parser.add_argument('-cuda', '--cuda', default='Y')
+    parser.add_argument('-type', '--type', type=int, default=0)
+
+
+    parser.add_argument('-lr', '--lr', type=float, default=0.001)
+    parser.add_argument('-wd', '--wd', type=float, default=0.00001)
+    parser.add_argument('-minlr', '--minlr', type=float, default=0.001)
+    parser.add_argument('-out', '--out', default=None, help="Output folder to put all files for training run")
+    parser.add_argument('-gamma', '--gamma', type=float, default=0.9)
+    parser.add_argument('-dataset', '--dataset', type=int, default=1)
+
+    args = parser.parse_args()
+
+    print("Beginning Training for model. Using the following parameters passed (Some default)\n")
+    print("\n{}".format(args))
+
+
+
+    # Import the dataset & get num of batches
+    train_dir = os.path.join(os.path.abspath(args.dir), 'train')
+    test_dir = os.path.join(os.path.abspath(args.dir), 'test')
+
+    train_transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((256, 256))])
+    kitti_train_dataset = KittiData.KittiROIDataset(train_dir, training=True, transform=train_transform)
+    #kitti_test_dataset = KittiData.KittiROIDataset(test_dir, training=False, transform=train_transform)
+
+    train_data = DataLoader(kitti_train_dataset, batch_size=args.b, shuffle=True)
+    #test_data = DataLoader(kitti_test_dataset, batch_size=args.b, shuffle=False)
+
+
+    encoder = model.encoder_decoder.encoder
+    if args.l is not None:
+        encoder.load_state_dict(torch.load(args.l))
+
+
+    # # Creating folder to store all files made during training
+    # if args.out != None:
+    #     if os.path.exists(os.path.abspath(args.out)):
+    #         print("Saving all files created to folder '{}'".format(args.out))
+    #     else:
+    #         os.mkdir(args.out)
+    #         print("Created folder {} to save all files made during training".format(args.out))
+    # else:
+    #     args.out = "./"
+
+    # Set the device (GPU if available, otherwise CPU)
+    if torch.cuda.is_available() and args.cuda == 'Y':
+        if torch.cuda.device_count() > 1:
+            device = torch.device("cuda:1")
+        else:
+            device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+
+    print("Using device: {}".format(device))
+
+    loss_fn = nn.functional.cross_entropy
+    model = model.model(encoder)
+    model.to(device)
+
+    # Define optimizer and learning rate scheduler
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=2, verbose=True, factor=0.1,
+                                                     min_lr=args.minlr)
+    # TODO: DELETE TEST BELOW
+    # print("Getting first img")
+    # for idx, data in enumerate(train_data):
+    #     print("IDX: {}    Img: {}      Label: {}".format(idx, data[0], data[1]))
+    #     break
+
+
+    # Train the model
+    train(args.e, optimizer, model, loss_fn, train_data, scheduler, device, args.s,
+           args.p, evaluate_epochs=False, folder=args.out, store_data=True)
